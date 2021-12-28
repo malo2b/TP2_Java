0bd01ce3f350ce7743fe7781610c93f7356f6dbc
malo2b <malo.girard@gmail.com>
2021-12-28 11:33:54 +0100

    Debut ex5

---
+++ src/com/ex5/Main.java
@@ -0,0 +1,22 @@
+package com.ex5;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Personne malo = new Personne("Girard", "Malo", 20);
+        Personne francois = new Personne("Focardi", "Francois-philippe", 20);
+        Personne pierrelouis = new Personne("Squaglia", "Pierre-louis", 20);
+        Personne nicolas = new Personne("Ottavi", "Nicolas", 33);
+        ArrayList<Personne> classe = new ArrayList<>();
+        classe.add(malo);
+        classe.add(francois);
+        classe.add(pierrelouis);
+        classe.add(nicolas);
+        System.out.println(classe);
+        classe.sort();
+    }
+
+}
+
---
+++ src/com/ex5/Personne.java
@@ -0,0 +1,80 @@
+package com.ex5;
+
+import java.util.Comparator;
+
+public class Personne implements Comparator<Personne> {
+
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Personne(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Compares its two arguments for order.  Returns a negative integer,
+     * zero, or a positive integer as the first argument is less than, equal
+     * to, or greater than the second.<p>
+     * <p>
+     * The implementor must ensure that {@link Integer#signum
+     * signum}{@code (compare(x, y)) == -signum(compare(y, x))} for
+     * all {@code x} and {@code y}.  (This implies that {@code
+     * compare(x, y)} must throw an exception if and only if {@code
+     * compare(y, x)} throws an exception.)<p>
+     * <p>
+     * The implementor must also ensure that the relation is transitive:
+     * {@code ((compare(x, y)>0) && (compare(y, z)>0))} implies
+     * {@code compare(x, z)>0}.<p>
+     * <p>
+     * Finally, the implementor must ensure that {@code compare(x,
+     * y)==0} implies that {@code signum(compare(x,
+     * z))==signum(compare(y, z))} for all {@code z}.
+     *
+     * @param o1 the first object to be compared.
+     * @param o2 the second object to be compared.
+     * @return a negative integer, zero, or a positive integer as the
+     * first argument is less than, equal to, or greater than the
+     * second.
+     * @throws NullPointerException if an argument is null and this
+     *                              comparator does not permit null arguments
+     * @throws ClassCastException   if the arguments' types prevent them from
+     *                              being compared by this comparator.
+     * @apiNote It is generally the case, but <i>not</i> strictly required that
+     * {@code (compare(x, y)==0) == (x.equals(y))}.  Generally speaking,
+     * any comparator that violates this condition should clearly indicate
+     * this fact.  The recommended language is "Note: this comparator
+     * imposes orderings that are inconsistent with equals."
+     */
+    @Override
+    public int compare(Personne o1, Personne o2) {
+        int result;
+        // Verification age
+        if (o1.getAge() - o2.getAge() != 0)
+            result = o1.getAge() - o2.getAge();
+        else { // Verification nom
+            if (o1.getNom().toLowerCase().compareTo(o2.getNom().toLowerCase()) != 0)
+                result = o1.getNom().toLowerCase().compareTo(o2.getNom().toLowerCase());
+            else // Verification prenom
+                if (o1.getPrenom().toLowerCase().compareTo(o2.getPrenom().toLowerCase()) != 0)
+                    result = o1.getPrenom().toLowerCase().compareTo(o2.getPrenom().toLowerCase());
+                else
+                    result = 0;
+        }
+        return result;
+    }
+}
